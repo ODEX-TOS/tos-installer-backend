6f453c8d095a66b8cd6927a5b9a2e787d4a418f0
F0xedb <tom@odex.be>
2019-08-26 14:45:12 +0200

    Initial flow

---
+++ main.py
@@ -0,0 +1,17 @@
+import argparser
+
+
+import chroot
+import disk
+import encryption
+import git
+import mount
+import network
+import packages
+import partition
+import shell
+import user
+
+
+
+print(shell.Command().GetStdout())
---
+++ shell.py
@@ -0,0 +1,16 @@
+import subprocess
+
+
+# A simple wrapper class that executes basic commands
+class Command:
+    def __init__(self, command="echo Hello World"):
+        self.command = command
+    # Executes a given command and returns the exit code
+    def GetReturnCode(self):
+        return subprocess.call(self.command, shell=True)  
+
+    def GetSubProcess(self):
+        return subprocess.Popen(self.command, shell=True, stdout=subprocess.PIPE)
+
+    def GetStdout(self):
+        return self.GetSubProcess().stdout.read()
