fb504790e3a40e9b8c78270e68d8068005f69776
F0xedb <tom@odex.be>
2019-08-26 17:54:42 +0200

    Show different examples on how the functions work

--- main.py
+++ main.py
@@ -12,19 +12,42 @@ import partition
 import shell
 import user
 import partitiontable as pt
+import script
 
-#disks=disk.getAllDisks()
-#for dsk in disks:
-#    print(dsk.toString())
-#    print("")
 
-parts = []
-parts.append(pt.PartitionInfo("boot", True, "1MiB", "200MiB", False))
-parts.append(pt.PartitionInfo("lvm", False, "800MiB", "100%", False))
+# Example on how to read the disk
+def diskExample():
+    disks=disk.getAllDisks()
+    for dsk in disks:
+        print(dsk.toString())
+        print("")
 
-table = pt.PartitionTable(pt.EPartitionTabel.MSDOS, "/dev/sda", parts)
 
-commands = table.generateCommand()
+def PartitionTableGenExample():
+    parts = []
+    parts.append(pt.PartitionInfo("boot", True, "1MiB", "200MiB", False))
+    parts.append(pt.PartitionInfo("lvm", False, "800MiB", "100%", False))
 
-for command in commands:
-    print(command)
+    table = pt.PartitionTable(pt.EPartitionTabel.MSDOS, "/dev/sda", parts)
+
+    commands = table.generateCommand()
+
+    for command in commands:
+        print(command)
+
+
+def SourceScriptExample():
+    print(script.executeExternalScript("shell.sh"))
+
+def NetworkConnectExample():
+    connection = network.Connector()
+    print("Do we have a network connection {}".format(connection.bIsConnected))
+    if not connection.bIsConnected:
+        connection.establishConnectionCommand()
+        print("Do we have a network connection {}".format(connection.bIsConnected))
+    if not connection.bIsConnected:
+        connection.establishConnection("promisI", "54FEF1E6")
+        print("Do we have a network connection {}".format(connection.bIsConnected))
+
+
+NetworkConnectExample()
