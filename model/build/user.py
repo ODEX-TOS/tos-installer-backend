783bd3197d51719d3c91e1210158e0357523ccc8
F0xedb <tom@odex.be>
2019-08-28 21:02:15 +0200

    User generator added

---
+++ model/build/user.py
@@ -0,0 +1,28 @@
+import config
+import crypt
+import os
+import sys
+sys.path.append("...")  # set all imports to root imports
+
+
+def makeUnixUser(user):
+    groups = listToString(user.groups)
+    return config.USERADD.format(getEncryptedPassword(user.password), groups, user.shell, user.name)
+
+
+def makeUnixUserHome(user):
+    return config.USER_HOME_BUILDER.format(user.name)
+
+
+def listToString(list, delimiter=" "):
+    string = ""
+    for item in list:
+        string += item + delimiter
+    # return the full string except the last delimiter since it doesn't delimit
+    # eg 1,2,3,4,
+    # to 1,2,3,4
+    return string[:-1]
+
+
+def getEncryptedPassword(password):
+    return crypt.crypt(password, "password")
