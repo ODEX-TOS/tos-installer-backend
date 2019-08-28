f0f0d6c6558babc0c0cdc37445709fed500eadb2
F0xedb <tom@odex.be>
2019-08-28 20:55:00 +0200

    Generate software

--- model/build/software.py
+++ model/build/software.py
@@ -1,5 +1,3 @@
-from model.model.software import software
-import config
 import shell
 import os
 import sys
@@ -15,16 +13,3 @@ def installSoftware(software):
         packagelist += package + " "
         obj = shell.Command(software.installer + " " + packagelist)
     return obj.GetReturnCode()
-
-
-def BuildSoftwareFromFile(file, installer=config.INSTALLCOMMAND):
-    """
-    Build a software object from a file containing packages
-    @file = a file containing packages (every line is a package)
-    @installer = the base install command to install software
-    """
-    sw = software(basecommand=installer)
-    with open('filename') as f:
-        lines = f.read().splitlines()
-    sw.packages = lines
-    return sw
