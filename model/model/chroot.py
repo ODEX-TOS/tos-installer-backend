a421d4eb304022db2e206ba1b656872f8c3b3807
F0xedb <tom@odex.be>
2019-09-01 23:48:27 +0200

    Added option to uses subherestring for users that are not root in chrooted environments

--- model/model/chroot.py
+++ model/model/chroot.py
@@ -12,6 +12,32 @@ class chroot:
         self.user = user
 
     def start(self, command="/bin/bash", herefile="EOF"):
+        """
+        Generate the chrooted environment commands. A chrooted environment should have a list of commands to execute (inside of this)
+        A herefile/string should be supplied to specify the delimiter as the underlying structure uses these. 
+        The herestring should be a string of charachters that is not presend inside the command
+        """
+        if self.user == "root":
+            return self.chrootAsRoot(command, herefile)
+        return self.chrootAsUser(command, herefile, herefile+"2")
+
+    def chrootAsUser(self, command, herefile, userHerefile):
+        """
+        This is an internal method and should not be used outside the scope of this class
+        Use start instead
+        """
+        if not type(command) is list:
+            return [self.chroot.format("root", self.mountpoint) + " <<{}\n".format(herefile) + "su {} <<\{}\n".format(self.user, userHerefile) + command + "\n" + userHerefile + "\n" + herefile]
+        commands = ""
+        for item in command:
+            commands += item + "\n"
+        return [self.chroot.format("root", self.mountpoint) + " <<{}\n".format(herefile) + "su {} <<\{}\n".format(self.user, userHerefile) + commands + "\n" + userHerefile + "\n" + herefile]
+
+    def chrootAsRoot(self, command, herefile):
+        """
+        This is an internal method and should not be used outside the scope of this class
+        Use start instead
+        """
         if not type(command) is list:
             return [self.chroot.format(self.user, self.mountpoint) + " <<{}\n".format(herefile) + command + "\n" + herefile]
         commands = ""
--- new.sh
+++ new.sh
@@ -1,20 +1,45 @@
 
 #Building partition table
-parted --script '/dev/sda' mklabel msdos
-parted --script '/dev/sda' mkpart primary 1MiB 200MiB
+parted --script '/dev/sda' mklabel gpt
+parted --script '/dev/sda' mkpart ESP fat32 1MiB 200MiB
 parted --script '/dev/sda' set 1 boot on
-parted --script '/dev/sda' name 1 boot
-parted --script '/dev/sda' mkpart primary 200MiB 100%
-parted --script '/dev/sda' name 2 root
+parted --script '/dev/sda' name 1 efi
+parted --script '/dev/sda' mkpart primary 200MiB 8GiB
+parted --script '/dev/sda' name 2 swap
+parted --script '/dev/sda' mkpart primary 8GiB 100%
+parted --script '/dev/sda' set 3 lvm on
+parted --script '/dev/sda' name 3 root
 
 #Formating partitions
-mkfs.ext4 -F /dev/sda1
+mkfs.fat -I -F32 /dev/sda1
 mkfs.ext4 -F /dev/sda2
+modprobe dm-crypt
+modprobe dm-mod
+cryptsetup luksFormat -v -s 512 -h sha512 /dev/sda3
+cryptsetup open /dev/sda3 luks_lvm
+pvcreate /dev/mapper/luks_lvm
+vgcreate /dev/mapper/luks_lvm
+lvcreate -n root -L 200G tos
+lvcreate -n home -L 200G tos
+mkfs.ext4 -L root /dev/mapper/tos-root
+mkfs.ext4 -L home /dev/mapper/tos-home
+
+#Formating partitions
+mkfs.fat -I -F32 /dev/sda1
 
 #Mounting partitions
-mount /dev/sda2 /mnt
-mkdir -p /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount /dev/mapper/tos-root /mnt/
+mount /dev/mapper/tos-home /mnt/home
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+swapon /dev/sda2
+swapon -a
+swapon -s
+
+#Establishing a network connection
+if [[ $(ping -c1 8.8.8.8 | grep '0% packet loss') == '' ]]; then
+	nmcli device wifi connect 'ssid' password 'passphrase'
+fi
 
 #bootstrapping system
 pacstrap /mnt base base-devel efibootmgr vim dialog grub  --noconfirm
@@ -23,7 +48,7 @@ pacstrap /mnt base base-devel efibootmgr vim dialog grub  --noconfirm
 genfstab -U -p /mnt > /mnt/etc/fstab
 
 # Executing chroot function
-cat | arch-chroot -u root /mnt <<EOF
+arch-chroot -u root /mnt <<EOF
 
 # Setting up system parameters
 timedatectl set-ntp true
@@ -32,11 +57,11 @@ sed -i 's:^#.*en_US.UTF-8:en_US.UTF-8:' /etc/locale.gen
 locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 echo KEYMAP='be-latin1' > /etc/vconsole.conf
-echo 'arch' > /etc/hostname
+echo 'tos' > /etc/hostname
 echo -e '127.0.0.1   localhost
 ::1      localhost
-127.0.1.1    arch.localdomain  arch' > /etc/hosts
-echo '123' | passwd
+127.0.1.1    tos.localdomain  tos' > /etc/hosts
+echo 'root:123' | chpasswd
 echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 pacman -Syu --noconfirm linux 
 
@@ -44,8 +69,29 @@ pacman -Syu --noconfirm linux
 useradd -m -p paN8aiEIonqJE -g users -G audio,lp,optical,storage,video,wheel,games,power -s /bin/bash alpha
 
 # Generating the bootloader
+sed -i 's:HOOKS=(\(.*\)):HOOKS=(\1 encrypt):' /etc/mkinitcpio.conf
 mkinitcpio -p linux
-grub-install /dev/sda
+grub-install --efi-directory /boot/efi --force /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
 EOF
+
+# Executing chroot function
+arch-chroot -u root /mnt <<EOF
+su alpha <<\EOF2
+
+# Installing software
+pacman -Syu --noconfirm linux grep vim linux-tos sudo nano 
+
+# Executing custom script
+ echo hello world
+echo hello 2
+
+
+EOF2
+EOF
+
+# Executing custom script
+ echo hello world
+echo hello 2
+
